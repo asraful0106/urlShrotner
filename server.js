fd05b8c0b127be32709e74262be5ba48d951c9f6
asraful0106 <asrafulapf@gmail.com>
2025-03-30 18:00:46 +0600

    Redircting route has been done!

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import viewRouter from './routes/view.routes.js';
 import uniqeUser from './middlewares/uniqeUser.js';
 import connectDB from './database/db.js';
 import creatUrlRouter from './routes/creatUrl.route.js';
+import shortUrlModel from './models/shortUrl.model.js';
 
 dotenv.config();
 
@@ -33,10 +34,29 @@ app.set('view engine', 'ejs');
 // Connect Databse
 connectDB();
 
+
+// For redirecting 
+app.get('/:uuid', async (req, res) => {
+    const userParam = req.params.uuid;
+    const { userAgent, ipAddress } = req.body;
+
+    try {
+        const url = await shortUrlModel.findOne({ shortenedUrl: userParam });
+        // console.log("URL: ", url);
+        if (!url) return res.render('pages/not_found');
+
+        url.recordClick(userAgent, ipAddress);
+        res.redirect(url.originalUrl);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error: err });
+    }
+});
+
 // Handeling the view routes
 app.use('/', uniqeUser, viewRouter); // Apply the middleware only to viewRouter
 
 // Creating Short Url
 app.use('/create', creatUrlRouter);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
